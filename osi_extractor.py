9a094f45d35180406a9df5bef73d5e7e4c76fc70
Raphael Schilling <raphael.schilling@tum.de>
2022-02-22 12:00:40 +0100

    add step wise curvature calculation

--- osi_extractor.py
+++ osi_extractor.py
@@ -1,14 +1,33 @@
 import sys
 import threading
 import math
+import random
 
 from osi3.osi_groundtruth_pb2 import GroundTruth
 from osi3.osi_lane_pb2 import Lane
 from osi3.osi_common_pb2 import Vector3d
-from typing import Dict
+from typing import Dict, List
 
 
 from  osi_iterator import OSI3GroundTruthIterator, UDPGroundTruthIterator
+
+def calc_curvature_for_lane(lane: Lane) -> List[float]:
+    centerline = lane.classification.centerline
+    curvatures = []
+    for i in range(len(centerline)):
+        if i == 0 or i == len(centerline) - 1:
+            curvatures.append(0.0)
+        else:
+            p1 = centerline[i-1] 
+            p2 = centerline[i]
+            p3 = centerline[i+1]
+            
+            a = euclidean_distance(p1, p2)
+            b = euclidean_distance(p2, p3)
+            c = euclidean_distance(p3, p1)
+            A = 1/4 * math.sqrt(4*a*a*b*b - (a*a + b*b -c*c)**2) # https://en.wikipedia.org/wiki/Heron%27s_formula
+            curvatures.append(4*A/(a*b*c)) # https://en.wikipedia.org/wiki/Menger_curvature
+    return curvatures
 
 def euclidean_distance(vec1: Vector3d, vec2: Vector3d, ignore_z: bool = True) -> float:
     if ignore_z:
@@ -45,6 +64,7 @@ def find_lane_pice_for_coord(lane: Lane, coordinate: Vector3d) -> int:
 
 class OSI3Extractor:
     lanes: Dict[int, Lane] = {}
+    lane_curvatures: Dict[int, List[float]] = {}
     def __init__(self, ip_addr: str, port: int = 48198):
         self.ground_truth_iterator = UDPGroundTruthIterator(ip_addr, port)
         self.thread = threading.Thread(target=self.thread_target)
@@ -64,13 +84,16 @@ class OSI3Extractor:
                         lane_id = object.assigned_lane_id[0].value
                     else:
                         lane_id = object.moving_object_classification.assigned_lane_id[0].value
-                    print("Id of closest lane piece: " + str(find_lane_pice_for_coord(self.lanes[lane_id], object.base.position)))
+                    closest_lane_pice = find_lane_pice_for_coord(self.lanes[lane_id], object.base.position)
+                    print("Id of closest lane piece: " + str(closest_lane_pice))
+                    print("curvature of closest piece: " + str(self.lane_curvatures[lane_id][closest_lane_pice]))
                     break
             print("----------------------------------------------------")
 
     def update_lanes(self, new_lanes: Lane):
         for l in new_lanes:
             self.lanes[l.id.value] = l
+            self.lane_curvatures[l.id.value] = calc_curvature_for_lane(l)
 
             
 def main():
