09684320827fed56df157991b77d92914f6ae79c
stovemeerkat <marvin.heinzelmann@t-online.de>
2022-03-21 16:56:00 +0100

    feat: Add classes to store data about lanes and lane boundaries

---
+++ lane.py
@@ -0,0 +1,62 @@
+import numpy as np
+from osi3.osi_lane_pb2 import Lane, LaneBoundary
+from osi3.osi_common_pb2 import Vector3d
+from osi3.osi_groundtruth_pb2 import GroundTruth
+
+from geometry import osi_vector_to_ndarray
+
+def get_lane_from_ground_truth(gt: GroundTruth, lane_id: int) -> Lane:
+    for lane in gt.lane:
+        if lane.id.value == lane_id:
+            return lane
+    raise RuntimeError(f"Missing data for lane {lane_id}")
+
+
+def get_lane_boundary_from_ground_truth(
+    gt: GroundTruth,
+    boundary_id: int
+) -> LaneBoundary:
+    for boundary in gt.lane_boundary:
+        if boundary.id.value == boundary_id:
+            return boundary
+    raise RuntimeError(f"Missing data for lane boundary {boundary_id}")
+
+
+class LaneState:
+    def __init__(self, gt: GroundTruth, lane_id: int):
+        self.osi_lane = get_lane_from_ground_truth(gt, lane_id)
+        left_boundaries = [
+            LaneBoundaryState(gt, id)
+            for id in self.osi_lane.classification.left_lane_boundary_id
+        ]
+        right_boundaries = [
+            LaneBoundaryState(gt, id)
+            for id in self.osi_lane.classification.right_lane_boundary_id
+        ]
+        n_left_segments = sum(len(b.segments) for b in left_boundaries)
+        n_right_segments = sum(len(b.segments) for b in right_boundaries)
+        self.left_start_points = np.empty((n_left_segments, 3))
+        self.left_end_points = np.empty((n_left_segments, 3))
+        i: int = 0
+        for left_boundary in left_boundaries:
+            for start, end in left_boundary.segments:
+                self.left_start_points[i,:] = start
+                self.left_end_points[i,:] = end
+                i += 1
+        self.right_start_points = np.empty((n_right_segments, 3))
+        self.right_end_points = np.empty((n_right_segments, 3))
+        i = 0
+        for right_boundary in right_boundaries:
+            for start, end in right_boundary.segments:
+                self.right_start_points[i,:] = start
+                self.right_end_points[i,:] = end
+                i += 1
+
+
+class LaneBoundaryState:
+    def __init__(self, gt: GroundTruth, boundary_id: int):
+        self.osi_boundary = get_lane_boundary_from_ground_truth(gt, boundary_id)
+        self.segments: list[tuple[np.ndarray, np.ndarray]] = []
+        line = self.osi_boundary.boudary_line
+        for i in range(len(line) - 1):
+            self.segments.append((line[i], line[i + 1]))
