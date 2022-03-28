9adc739e21061342e47379c7e0b1236f904024b1
Raphael Schilling <raphael.schilling@tum.de>
2022-03-28 13:41:09 +0200

    curvature works

--- curvature.py
+++ curvature.py
@@ -1,40 +1,54 @@
 from typing import List
 from osi3.osi_lane_pb2 import Lane
+from osi3.osi_common_pb2 import Vector3d
 from geometry import euclidean_distance
 import math
+import numpy as np
 
-def get_road_curvature(self):
-    lane_id = self._get_ego_lane_id()
-    piece_id, percentage = find_lane_piece_for_coord(self.lanes[lane_id], self.host_vehicle.base.position, return_progress=True) 
-    return self.lane_curvatures[lane_id][piece_id]*(1-percentage) + self.lane_curvatures[lane_id][piece_id+1]*percentage
 
-def get_road_curvature_change(self):
-    lane_id = self._get_ego_lane_id()
-    piece_id = find_lane_piece_for_coord(self.lanes[lane_id], self.host_vehicle.base.position) 
+class Curvatrure():
+    def __init__(self, centerline: np.ndarray, distances: np.ndarray):
+        #        self._curvature_list = np.array(self._calc_curvature_for_lane_slow(lane))
+        self._curvature_list = self._calc_curvature_for_line(centerline)
+#        if not np.allclose(self._curvature_change_list, self._calc_curvature_for_line(line)):
+#            raise RuntimeError("Wrong curvatrure computation")
+        self._curvature_change_list = self._calc_curvature_change_list(
+            self._curvature_list, distances)
 
-    curvature_difference = self.lane_curvatures[lane_id][piece_id + 1] - self.lane_curvatures[lane_id][piece_id]
-    piece_length = euclidean_distance(self.lanes[lane_id].classification.centerline[piece_id], 
-    self.lanes[lane_id].classification.centerline[piece_id + 1])
-    return curvature_difference / piece_length
+    def get_road_curvature(self, segment_index: int, segment_process: float):
+        return self._curvature_list[segment_index]*(1-segment_process) + self._curvature_list[segment_index+1]*segment_process
 
-def calc_curvature_for_lane(lane: Lane) -> List[float]:
-    centerline = lane.classification.centerline
-    curvatures = []
-    for i in range(len(centerline)):
-        if i == 0 or i == len(centerline) - 1:
-            curvatures.append(0.0)
-        else:
-            p1 = centerline[i-1]
-            p2 = centerline[i]
-            p3 = centerline[i+1]
+    def get_road_curvature_change(self, segment_index: int):
+        return self._curvature_change_list[segment_index]
 
-            a = euclidean_distance(p1, p2)
-            b = euclidean_distance(p2, p3)
-            c = euclidean_distance(p3, p1)
-            try:
-                A = 1/4 * math.sqrt(4*a*a*b*b - (a*a + b*b -c*c)**2) # https://en.wikipedia.org/wiki/Heron%27s_formula
-            except ValueError:
-                A = 0.0
-            curvatures.append(4*A/(a*b*c)) # https://en.wikipedia.org/wiki/Menger_curvature
-    return curvatures
+    def _calc_curvature_change_list(self, curvatures: np.ndarray, distances: np.ndarray):
+        curvature_differences = curvatures[1:] - curvatures[:-1]
+        curvature_changes = curvature_differences / distances
+        return curvature_changes
 
+    def _calc_curvature_for_line(self, centerline: np.ndarray) -> np.ndarray:
+        c_0 = centerline[:-2, :]
+        c_1 = centerline[1:-1, :]
+        c_2 = centerline[2:, :]
+        a_np = np.linalg.norm(
+            c_1 - c_0,
+            axis=1,
+        )
+        b_np = np.linalg.norm(
+            c_2 - c_1,
+            axis=1,
+        )
+        c_np = np.linalg.norm(
+            c_2 - c_0,
+            axis=1,
+        )
+        a_squared = np.square(a_np)
+        b_squared = np.square(b_np)
+        # https://en.wikipedia.org/wiki/Heron%27s_formula
+        A = 1/4 * np.sqrt(np.maximum(0, 4*a_squared*b_squared -
+                          np.square(a_squared + b_squared - np.square(c_np))))
+
+        ret_val = np.zeros((centerline.shape[0],))
+        # https://en.wikipedia.org/wiki/Menger_curvature
+        ret_val[1:-1] = 4*A/(a_np*b_np*c_np)
+        return ret_val
