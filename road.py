d6d1f1981e0daea63cf0a51d5be752ccc5d9e19d
Raphael Schilling <raphael.schilling@tum.de>
2022-09-24 10:09:29 +0200

    auto format road.py

--- road.py
+++ road.py
@@ -11,21 +11,25 @@ SUBTYPE_ENTRY = lane_pb2._LANE_CLASSIFICATION_SUBTYPE.values_by_name["SUBTYPE_EN
 SUBTYPE_EXIT = lane_pb2._LANE_CLASSIFICATION_SUBTYPE.values_by_name["SUBTYPE_EXIT"].number
 SUBTYPE_NORMAL = lane_pb2._LANE_CLASSIFICATION_SUBTYPE.values_by_name["SUBTYPE_NORMAL"].number
 SUBTYPE_ONRAMP = lane_pb2._LANE_CLASSIFICATION_SUBTYPE.values_by_name["SUBTYPE_ONRAMP"].number
-SUBTYPE_OFFRAMP = lane_pb2._LANE_CLASSIFICATION_SUBTYPE.values_by_name["SUBTYPE_OFFRAMP"].number
-SUBTYPE_CONNECTINGRAMP = lane_pb2._LANE_CLASSIFICATION_SUBTYPE.values_by_name["SUBTYPE_CONNECTINGRAMP"].number
+SUBTYPE_OFFRAMP = lane_pb2._LANE_CLASSIFICATION_SUBTYPE.values_by_name[
+    "SUBTYPE_OFFRAMP"].number
+SUBTYPE_CONNECTINGRAMP = lane_pb2._LANE_CLASSIFICATION_SUBTYPE.values_by_name[
+    "SUBTYPE_CONNECTINGRAMP"].number
+
 
 class Road:
     road_id: int
-    _rightmost_lanes: list[LaneGraphNode]  
+    _rightmost_lanes: list[LaneGraphNode]
     _rightmost_lanes_lengths: np.ndarray
     _total_distance: float
 
     def _get_rightmost_roadlane(self, lane: LaneGraphNode) -> LaneGraphNode:
         current_lane = lane
-        while(current_lane not in self._rightmost_lanes):
+        while current_lane not in self._rightmost_lanes:
             current_lane = current_lane.right
             if current_lane is None:
-                raise Exception(f"Lane with id:{lane.id} seems to be not part of road with id: {self.road_id}")
+                raise Exception(
+                    f"Lane with id:{lane.id} seems to be not part of road with id: {self.road_id}")
         return current_lane
 
     def object_road_s(self, lane: LaneGraphNode, position: np.ndarray) -> float:
@@ -38,11 +42,11 @@ class Road:
 
 
 class RoadManager:
-    lane_id_to_road_map: dict[int, Road] = {} 
+    lane_id_to_road_map: dict[int, Road] = {}
 
     def __init__(self, lane_graph: LaneGraph) -> None:
         self._create_roads(lane_graph)
-    
+
     def _get_next_mostright_lane(self, old_level_lane: LaneGraphNode) -> Optional[LaneGraphNode]:
         current_old_level_lane = old_level_lane
         while current_old_level_lane is not None:
@@ -50,11 +54,13 @@ class RoadManager:
             if successor is not None:
                 current_new_level_lane = successor
                 while True:
-                    next_new_level_lane = self._same_road_right_neighbor(current_new_level_lane)
+                    next_new_level_lane = self._same_road_right_neighbor(
+                        current_new_level_lane)
                     if next_new_level_lane is None:
                         return current_new_level_lane
                     current_new_level_lane = next_new_level_lane
-            current_old_level_lane = self._same_road_left_neighbor(current_old_level_lane)
+            current_old_level_lane = self._same_road_left_neighbor(
+                current_old_level_lane)
         return None
 
     def _add_all_parallel_lanes_to_road(self, rightmost_lane: LaneGraphNode, road: Road):
@@ -62,7 +68,6 @@ class RoadManager:
         while lane is not None:
             self.lane_id_to_road_map[lane.id] = road
             lane = self._same_road_left_neighbor(lane)
-            
 
     def _create_new_road_starting_with(self, lane: LaneGraphNode, road_id: int):
         new_road = Road()
@@ -80,8 +85,8 @@ class RoadManager:
         new_road._rightmost_lanes_lengths = np.array(temp_length)
         new_road._total_distance = np.sum(new_road._rightmost_lanes_lengths)
 
-    
-    def _same_road_right_neighbor(self, lane: LaneGraphNode) -> Optional[LaneGraphNode]: #TODO: Don't know if this is good
+    # TODO: Don't know if this is good
+    def _same_road_right_neighbor(self, lane: LaneGraphNode) -> Optional[LaneGraphNode]:
         road_independent_neighbor = lane.right
         if road_independent_neighbor is None:
             return None
@@ -92,7 +97,8 @@ class RoadManager:
                 return None
             return road_independent_neighbor
 
-    def _same_road_left_neighbor(self, lane: LaneGraphNode) -> Optional[LaneGraphNode]: #TODO: Don't know if this is good
+    # TODO: Don't know if this is good
+    def _same_road_left_neighbor(self, lane: LaneGraphNode) -> Optional[LaneGraphNode]:
         road_independent_neighbor = lane.left
         if road_independent_neighbor is None:
             return None
@@ -103,7 +109,8 @@ class RoadManager:
                 return None
             return road_independent_neighbor
 
-    def _same_road_successor(self, lane: LaneGraphNode) -> Optional[LaneGraphNode]: #TODO: Don't know if this is good
+    # TODO: Don't know if this is good
+    def _same_road_successor(self, lane: LaneGraphNode) -> Optional[LaneGraphNode]:
         road_independent_successor = lane.successor
         if road_independent_successor is None:
             return None
@@ -114,14 +121,15 @@ class RoadManager:
                 return None
             if successor_subtype == lane_subtype:
                 return road_independent_successor
-            
+
             if lane_subtype == SUBTYPE_EXIT and successor_subtype != SUBTYPE_EXIT:
                 return None
             if lane_subtype != SUBTYPE_ENTRY and successor_subtype == SUBTYPE_ENTRY:
                 return None
             return road_independent_successor
 
-    def _same_road_precessor(self, lane: LaneGraphNode) -> Optional[LaneGraphNode]: #TODO: Don't know if this is good
+    # TODO: Don't know if this is good
+    def _same_road_precessor(self, lane: LaneGraphNode) -> Optional[LaneGraphNode]:
         road_independent_precessor = lane.predecessor
         if road_independent_precessor is None:
             return None
@@ -132,13 +140,12 @@ class RoadManager:
                 return None
             if precessor_subtype == lane_subtype:
                 return road_independent_precessor
-            
+
             if precessor_subtype == SUBTYPE_EXIT and lane_subtype != SUBTYPE_EXIT:
                 return None
             if precessor_subtype != SUBTYPE_ENTRY and lane_subtype == SUBTYPE_ENTRY:
                 return None
             return road_independent_precessor
-
 
     def _is_rightmost_beginning_lane(self, lane: LaneGraphNode) -> bool:
         if self._same_road_right_neighbor(lane) is not None:
@@ -150,7 +157,6 @@ class RoadManager:
             current_lane = self._same_road_left_neighbor(current_lane)
         return True
 
-
     def _create_roads(self, lane_graph: LaneGraph):
         next_road_id = 0
         for lane_id, lane in lane_graph._nodes.items():
@@ -160,12 +166,8 @@ class RoadManager:
                 self._create_new_road_starting_with(lane, next_road_id)
                 next_road_id += 1
 
-
-
-
-            
     def get_road_id(self, lane: LaneGraphNode) -> Optional[int]:
         return self.lane_id_to_road_map[lane.id].road_id
+
     def object_road_s(self, lane: LaneGraphNode, position: np.ndarray) -> float:
         return self.lane_id_to_road_map[lane.id].object_road_s(lane, position)
-        
