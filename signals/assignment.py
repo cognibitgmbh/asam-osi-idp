7d6bd08d085e5232e944fe723b665fe4026e6988
stovemeerkat <marvin.heinzelmann@t-online.de>
2023-01-25 18:48:48 +0100

    feat(traffic_signals): Consider angle between signal and road - if a signal is rotated by more than 45 degrees relative to a lane, it can not be assigned to that lane/road

--- signals/assignment.py
+++ signals/assignment.py
@@ -1,15 +1,25 @@
+import math
+from collections.abc import Mapping
+from types import MappingProxyType
 from typing import Optional
 
 import numpy as np
 from osi3.osi_groundtruth_pb2 import GroundTruth
 
-from collections.abc import Mapping
-from types import MappingProxyType
-
-from ..geometry import euclidean_distance, osi_vector_to_ndarray
+from .roadsignal import RoadSignal
+from ..geometry import angle_between_vectors, euclidean_distance, Orientation, osi_vector_to_ndarray, ProjectionResult
 from ..lanegraph import LaneGraph, LaneGraphNode
 from ..road import RoadManager
-from .roadsignal import RoadSignal
+
+
+SIGN_VIEW_NORMAL = np.array([1, 0, 0])
+SIGN_MAX_ANGLE = math.pi / 4.0          # 45 degrees
+
+
+def lane_check_sign_orientation(lane: LaneGraphNode, orientation: Orientation, projection: ProjectionResult) -> bool:
+    lane_point1, lane_point2 = lane.data.segment_points(projection.segment_index)
+    lane_reverse_direction = orientation.rotate_vector(lane_point1 - lane_point2)
+    return angle_between_vectors(lane_reverse_direction, SIGN_VIEW_NORMAL) <= SIGN_MAX_ANGLE
 
 
 class RoadAssignmentBuilder:
@@ -25,7 +35,7 @@ class RoadAssignmentBuilder:
         for osi_sign in gt.traffic_sign:
             main_sign_base = osi_sign.main_sign.base
             position = osi_vector_to_ndarray(main_sign_base.position)
-            orientation = osi_vector_to_ndarray(main_sign_base.orientation)
+            orientation = Orientation.from_osi(main_sign_base.orientation)
             lane = self._find_closest_lane(position, orientation)
             if lane is None:
                 print(f'WARNING: Could not assign traffic sign {osi_sign.id.value} to a lane')
@@ -43,13 +53,14 @@ class RoadAssignmentBuilder:
                 assignment[road.road_id].append(road_signal)
         return MappingProxyType(assignment)
 
-    def _find_closest_lane(self, position: np.ndarray, orientation: np.ndarray) -> Optional[LaneGraphNode]:
+    def _find_closest_lane(self, position: np.ndarray, orientation: Orientation) -> Optional[LaneGraphNode]:
         closest_lane = None
         closest_distance = float('inf')
         for lane in self.lane_graph.iterate_nodes():
-            # TODO: filter by orientation
-            projection = lane.data.project_onto_centerline(position).projected_point
-            distance = euclidean_distance(position, projection, ignore_z=True)
+            projection = lane.data.project_onto_centerline(position)
+            if not lane_check_sign_orientation(lane, orientation, projection):
+                continue
+            distance = euclidean_distance(position, projection.projected_point, ignore_z=True)
             if closest_lane is None or distance < closest_distance:
                 closest_lane = lane
                 closest_distance = distance
